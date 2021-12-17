eabf006c0cbe1cbb4a38a7da848abaddde7a79ec
DuyLeVu <levuduy132@gmail.com>
2021-12-17 17:11:52 +0700

    customer-management-stored-procedure

---
+++ exercise-jpa/customer-management-stored-procedure/src/main/java/service/CustomerService.java
@@ -0,0 +1,17 @@
+package service;
+
+import model.Customer;
+import repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CustomerService implements ICustomerService {
+    @Autowired
+    private ICustomerRepository customerRepository;
+
+    @Override
+    public boolean insertWithStoredProcedure(Customer customer) {
+        return customerRepository.insertWithStoredProcedure(customer);
+    }
+}
